205a09941dd42ed331966c5a0ce09e110f9e45ba
webdevstar <webdeveloper1497@gmail.com>
2018-10-19 21:06:11 +0800

    upload

--- src/admin/client/modules/apps/reducer.js
+++ src/admin/client/modules/apps/reducer.js
@@ -1,73 +1,34 @@
 import * as t from './actionTypes';
 
 const initialState = {
-	editCustomer: null,
-	items: [],
-	selected: [],
-	hasMore: false,
-	totalCount: 0,
-	loadingItems: false,
-	errorLoadingItems: null,
-	search: ''
+	account: null,
+	services: [],
+	service: null,
+	serviceSettings: null,
+	serviceLogs: null,
+	loadingEnableDisableService: false
 };
 
 export default (state = initialState, action) => {
 	switch (action.type) {
-		case t.CUSTOMERS_DETAIL_REQUEST:
-			return Object.assign({}, state, {});
-		case t.CUSTOMERS_DETAIL_RECEIVE:
+		case t.ACCOUNT_RECEIVE:
+			return Object.assign({}, state, { account: action.account });
+		case t.SERVICES_RECEIVE:
+			return Object.assign({}, state, { services: action.services });
+		case t.SERVICE_RECEIVE:
+			return Object.assign({}, state, { service: action.service });
+		case t.SERVICE_SETTINGS_REQUEST:
+			return Object.assign({}, state, { serviceSettings: null });
+		case t.SERVICE_SETTINGS_RECEIVE:
 			return Object.assign({}, state, {
-				editCustomer: action.item
+				serviceSettings: action.serviceSettings
 			});
-		case t.CUSTOMERS_REQUEST:
-			return Object.assign({}, state, {
-				loadingItems: true
-			});
-		case t.CUSTOMERS_RECEIVE:
-			return Object.assign({}, state, {
-				loadingItems: false,
-				hasMore: action.has_more,
-				totalCount: action.total_count,
-				items: action.data
-			});
-		case t.CUSTOMERS_FAILURE:
-			return Object.assign({}, state, {
-				loadingItems: false,
-				errorLoadingItems: action.error
-			});
-		case t.CUSTOMERS_SELECT:
-			return Object.assign({}, state, {
-				selected: [...state.selected, action.customerId]
-			});
-		case t.CUSTOMERS_DESELECT:
-			return Object.assign({}, state, {
-				selected: state.selected.filter(id => id !== action.customerId)
-			});
-		case t.CUSTOMERS_DESELECT_ALL:
-			return Object.assign({}, state, {
-				selected: []
-			});
-		case t.CUSTOMERS_SELECT_ALL:
-			let selected = state.items.map(item => item.id);
-			return Object.assign({}, state, {
-				selected: selected
-			});
-		case t.CUSTOMERS_FILTER_SET_SEARCH:
-			return Object.assign({}, state, {
-				search: action.search
-			});
-		case t.CUSTOMERS_MORE_REQUEST:
-			return Object.assign({}, state, {
-				loadingItems: true
-			});
-		case t.CUSTOMERS_MORE_RECEIVE:
-			return Object.assign({}, state, {
-				loadingItems: false,
-				hasMore: action.has_more,
-				totalCount: action.total_count,
-				items: [...state.items, ...action.data]
-			});
-		case t.CUSTOMER_DELETE_SUCCESS:
+		case t.SERVICE_LOGS_RECEIVE:
+			return Object.assign({}, state, { serviceLogs: action.serviceLogs });
+		case t.SERVICE_ENABLE_REQUEST:
+			return Object.assign({}, state, { loadingEnableDisableService: true });
+		case t.SERVICE_ENABLE_RECEIVE:
+			return Object.assign({}, state, { loadingEnableDisableService: false });
 		default:
 			return state;
 	}
