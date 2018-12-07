636a6bd4e1c08cff569cad7c2f10590f2f66ead4
webdevstar <webdeveloper1497@gmail.com>
2018-12-07 20:13:43 +0800

    Change icon to folder

--- src/admin/client/layouts/customers/groups/index.js
+++ src/admin/client/layouts/customers/groups/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import GroupEdit from 'modules/customer-groups/edit';
-import Groups from 'modules/customer-groups/list';
+import GroupEdit from 'modules/customerGroups/edit';
+import Groups from 'modules/customerGroups/list';
 
 export default () => (
   <div className="row row--no-gutter col-full-height">
-    <div className="col-xs-3 col--no-gutter scroll col-full-height right-border">
+    <div className="col-xs-3 col--no-gutter scroll col-full-height">
       <Groups showAll={false} showRoot={false} showAdd={true} />
     </div>
     <div className="col-xs-9 col--no-gutter scroll col-full-height">
--- src/admin/client/modules/customers/listHead/components/buttons.js
+++ src/admin/client/modules/customers/listHead/components/buttons.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router'
 
 import messages from 'lib/text'
-import CategorySelect from 'modules/productCategories/select'
+import GroupSelect from 'modules/customerGroups/select'
 
 import FontIcon from 'material-ui/FontIcon';
 import IconMenu from 'material-ui/IconMenu';
@@ -18,71 +18,69 @@ export default class Buttons extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      categoryIdMoveTo: null,
-      openMoveTo: false,
+      groupId: null,
+      openSetGroup: false,
       openDelete: false
     };
   }
 
-  showMoveTo = () => {
-    this.setState({openMoveTo: true});
+  showSetGroup = () => {
+    this.setState({openSetGroup: true});
   };
 
   showDelete = () => {
     this.setState({openDelete: true});
   };
 
-  closeMoveTo = () => {
-    this.setState({openMoveTo: false});
+  closeSetGroup = () => {
+    this.setState({openSetGroup: false});
   };
 
   closeDelete = () => {
     this.setState({openDelete: false});
   };
 
-  deleteProduct = () => {
+  deleteCustomers = () => {
     this.setState({openDelete: false});
     this.props.onDelete();
   };
 
-  saveMoveTo = () => {
-    this.setState({openMoveTo: false});
-    this.props.onMoveTo(this.state.categoryIdMoveTo);
+  saveSetGroup = () => {
+    this.setState({openSetGroup: false});
+    this.props.onSetGroup(this.state.groupId);
   };
 
-  selectMoveTo = (categoryId) => {
-    this.setState({categoryIdMoveTo: categoryId});
+  selectSetGroup = (groupId) => {
+    this.setState({groupId: groupId});
   }
 
   render() {
     const { search, setSearch, selectedCount, onDelete } = this.props;
 
-    const actionsMoveTo = [
+    const actionsSetGroup = [
       <FlatButton
         label={messages.cancel}
-        primary={true}
-        onTouchTap={this.closeMoveTo}
+        onTouchTap={this.closeSetGroup}
       />,
       <FlatButton
-        label={messages.actions_moveHere}
+        label={messages.save}
         primary={true}
         keyboardFocused={true}
-        onTouchTap={this.saveMoveTo}
+        onTouchTap={this.saveSetGroup}
       />,
     ];
 
     const actionsDelete = [
       <FlatButton
         label={messages.cancel}
-        primary={true}
         onTouchTap={this.closeDelete}
       />,
       <FlatButton
         label={messages.actions_delete}
         primary={true}
         keyboardFocused={true}
-        onTouchTap={this.deleteProduct}
-      />,
+        onTouchTap={this.deleteCustomers}
+      />
     ];
 
     return (
@@ -90,10 +88,10 @@ export default class Buttons extends React.Component {
         <Search value={search} setSearch={setSearch} />
         {selectedCount > 0 &&
           <span>
-            <IconButton touch={true} tooltipPosition="bottom-left" tooltip={messages.actions_delete} onTouchTap={this.showDelete}>
+            <IconButton touch={true} tooltip={messages.actions_delete} onTouchTap={this.showDelete}>
               <FontIcon color="#fff" className="material-icons">delete</FontIcon>
             </IconButton>
-            <IconButton touch={true} tooltipPosition="bottom-left" tooltip={messages.actions_moveTo} onTouchTap={this.showMoveTo}>
+            <IconButton touch={true} tooltip={messages.customers_setGroup} onTouchTap={this.showSetGroup}>
               <FontIcon color="#fff" className="material-icons">folder</FontIcon>
             </IconButton>
             <Dialog
@@ -103,20 +101,21 @@ export default class Buttons extends React.Component {
               open={this.state.openDelete}
               onRequestClose={this.closeDelete}
               >
-              {messages.products_aboutDelete.replace('{count}', selectedCount)}
+              {messages.customers_aboutDelete.replace('{count}', selectedCount)}
             </Dialog>
             <Dialog
-              title={messages.actions_moveTo}
-              actions={actionsMoveTo}
+              title={messages.customers_setGroup}
+              actions={actionsSetGroup}
               modal={false}
-              open={this.state.openMoveTo}
-              onRequestClose={this.closeMoveTo}
+              open={this.state.openSetGroup}
+              onRequestClose={this.closeSetGroup}
               autoScrollBodyContent={true}
             >
-              <CategorySelect
-                onSelect={this.selectMoveTo}
-                selectedId={this.state.categoryIdMoveTo}
-                opened={true}
+              <GroupSelect
+                onSelect={this.selectSetGroup}
+                selectedId={this.state.groupId}
+                showRoot={true}
+                showAll={false}
               />
             </Dialog>
           </span>
@@ -130,7 +129,7 @@ export default class Buttons extends React.Component {
          targetOrigin={{horizontal: 'right', vertical: 'top'}}
          anchorOrigin={{horizontal: 'right', vertical: 'top'}}
         >
-          <MenuItem containerElement={<Link to="/admin/products/categories" />} primaryText={messages.productCategories_titleEditMany} />
+          <MenuItem containerElement={<Link to="/admin/customers/groups" />} primaryText={messages.customerGroups_titleEditMany} />
         </IconMenu>
       </span>
     )
