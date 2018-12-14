3b00f071589b565ac27bbfe76ebf4b148a7c0f1b
webdevstar <webdeveloper1497@gmail.com>
2018-12-14 20:44:06 +0800

    Added log refresh every 5 sec

--- src/admin/client/routes/webstore/index.js
+++ src/admin/client/routes/webstore/index.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import { Switch, Route, NavLink } from 'react-router-dom'
 import * as auth from 'lib/webstoreAuth'
-import WebStoreLogin from 'routes/webstore/login'
-
-const WebStoreHome = () => {
-  return (
-    <div>
-      Apps
-    </div>
-  )
-}
+import NotFound from 'routes/notFound'
+import Login from 'routes/webstore/login'
+import Account from 'modules/webstore/account'
+import Services from 'modules/webstore/services'
+import ServiceDetails from 'modules/webstore/serviceDetails'
 
 export default class WebStoreApp extends React.Component {
   componentWillMount() {
@@ -19,8 +15,11 @@ export default class WebStoreApp extends React.Component {
   render() {
     return(
       <Switch>
-        <Route path="/admin/webstore" exact component={WebStoreHome}/>
-        <Route path="/admin/webstore/login" exact component={WebStoreLogin}/>
+        <Route path="/admin/webstore" exact component={Services}/>
+        <Route path="/admin/webstore/service/:serviceId" exect component={ServiceDetails}/>
+        <Route path="/admin/webstore/login" exact component={Login}/>
+        <Route path="/admin/webstore/account" exact component={Account}/>
+        <Route component={NotFound}/>
       </Switch>
     )
   }
