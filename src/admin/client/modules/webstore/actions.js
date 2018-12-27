0942fa136134f100121376c174ade370a260de96
webdevstar <webdeveloper1497@gmail.com>
2018-12-27 21:14:14 +0800

    Added service details

--- src/admin/client/modules/webstore/actions.js
+++ src/admin/client/modules/webstore/actions.js
@@ -7,6 +7,16 @@ const receiveAccount = account => ({
   account
 })
 
+const receiveServices = services => ({
+  type: t.SERVICES_RECEIVE,
+  services
+})
+
+const receiveService = service => ({
+  type: t.SERVICE_RECEIVE,
+  service
+})
+
 export const fetchAccount = () => (dispatch, getState) => {
   return api.webstore.account.retrieve()
   .then(({status, json}) => {
@@ -25,5 +35,19 @@ export const updateDeveloperAccount = account => (dispatch, getState) => {
   return api.webstore.account.updateDeveloper(account)
   .then(({status, json}) => {
     dispatch(receiveAccount(json))
+  })
+}
+
+export const fetchServices = () => (dispatch, getState) => {
+  return api.webstore.services.list()
+  .then(({status, json}) => {
+    dispatch(receiveServices(json))
+  })
+}
+
+export const fetchService = (serviceId) => (dispatch, getState) => {
+  return api.webstore.services.retrieve(serviceId)
+  .then(({status, json}) => {
+    dispatch(receiveService(json))
   })
 }
--- src/admin/client/modules/webstore/reducer.js
+++ src/admin/client/modules/webstore/reducer.js
@@ -1,13 +1,19 @@
 import * as t from './actionTypes'
 
 const initialState = {
-  account: {}
+  account: {},
+  services: [],
+  service: null
 };
 
 export default(state = initialState, action) => {
   switch (action.type) {
     case t.ACCOUNT_RECEIVE:
       return Object.assign({}, state, {account: action.account})
+    case t.SERVICES_RECEIVE:
+      return Object.assign({}, state, {services: action.services})
+    case t.SERVICE_RECEIVE:
+      return Object.assign({}, state, {service: action.service})
     default:
       return state
   }
