19a3dd0a253ba2aba7f69d8851a03b9d2f7ccaa5
webdevstar <webdeveloper1497@gmail.com>
2019-01-12 23:06:42 +0800

    Refactoring

--- src/admin/client/app.js
+++ src/admin/client/app.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import * as auth from 'lib/auth'
 
 import Head from 'modules/head'
 import Login from 'routes/login'
@@ -17,7 +16,7 @@ import Orders from 'routes/orders'
 import OrderDetails from 'routes/orders/edit'
 import OrderStatuses from 'routes/orders/statuses'
 import Settings from 'routes/settings'
-import WebStore from 'routes/webstore'
+import Apps from 'routes/apps'
 
 import {blue700, cyan700, pinkA200, grey100, grey300, grey400, white, darkBlack, fullBlack} from 'material-ui/styles/colors';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
@@ -42,41 +41,33 @@ const muiTheme = getMuiTheme({
   appBar: {}
 });
 
-export default class App extends React.Component {
-  componentWillMount() {
-    auth.validateCurrentToken();
-  }
-
-  render() {
-    return(
-      <Router>
-          <MuiThemeProvider muiTheme={muiTheme}>
-            <div id="container">
-              <div id="headContainer">
-                <Head />
-              </div>
-              <div id="bodyContainer">
-                <Switch>
-                  <Route path="/admin/" exact component={Home}/>
-                  <Route path="/admin/login" component={Login} />
-                  <Route path="/admin/logout" component={Logout} />
-                  <Route path="/admin/products" exact component={Products}/>
-                  <Route path="/admin/products/categories" exact component={ProductCategories}/>
-                  <Route path="/admin/orders" exact component={Orders}/>
-                  <Route path="/admin/orders/statuses" exact component={OrderStatuses}/>
-                  <Route path="/admin/order/:orderId" exact component={OrderDetails}/>
-                  <Route path="/admin/customers" exact component={Customers}/>
-                  <Route path="/admin/customers/groups" exact component={CustomerGroups}/>
-                  <Route path="/admin/customer/:customerId" exact component={CustomerDetails}/>
-                  <Route path="/admin/product/:productId" component={ProductDetails}/>
-                  <Route path="/admin/settings" component={Settings}/>
-                  <Route path="/admin/webstore" component={WebStore}/>
-                  <Route component={NotFound}/>
-                </Switch>
-              </div>
-            </div>
-          </MuiThemeProvider>
-      </Router>
-    )
-  }
-}
+export default () => (
+  <Router>
+    <MuiThemeProvider muiTheme={muiTheme}>
+      <div id="container">
+        <div id="headContainer">
+          <Head />
+        </div>
+        <div id="bodyContainer">
+          <Switch>
+            <Route path="/admin/" exact component={Home}/>
+            <Route path="/admin/login" component={Login} />
+            <Route path="/admin/logout" component={Logout} />
+            <Route path="/admin/products" exact component={Products}/>
+            <Route path="/admin/products/categories" exact component={ProductCategories}/>
+            <Route path="/admin/orders" exact component={Orders}/>
+            <Route path="/admin/orders/statuses" exact component={OrderStatuses}/>
+            <Route path="/admin/order/:orderId" exact component={OrderDetails}/>
+            <Route path="/admin/customers" exact component={Customers}/>
+            <Route path="/admin/customers/groups" exact component={CustomerGroups}/>
+            <Route path="/admin/customer/:customerId" exact component={CustomerDetails}/>
+            <Route path="/admin/product/:productId" component={ProductDetails}/>
+            <Route path="/admin/settings" component={Settings}/>
+            <Route path="/admin/apps" component={Apps}/>
+            <Route component={NotFound}/>
+          </Switch>
+        </div>
+      </div>
+    </MuiThemeProvider>
+  </Router>
+)
